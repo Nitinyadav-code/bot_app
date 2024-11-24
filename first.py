ed62c4852e566500f4fd6980a1a9f744c5f65965
Nitinyadav-code <168860833+Nitinyadav-code@users.noreply.github.com>
2024-11-25 01:21:04 +0530

    Add files via upload

--- first.py
+++ first.py
@@ -8,6 +8,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Welcome to my mini-app!  write  /meme for meme')
 async def meme(update:Update, context: ContextTypes.DEFAULT_TYPE)-> None:
     await update.message.reply_text("serach @hasle_kallu on instagram" )
+async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    web_app_url = "https://youtube-download-rb1x.onrender.com"
+    await update.message.reply_text(f"Please use the web app to download videos: {web_app_url}")
+
     
 
 
@@ -17,6 +21,7 @@ application = Application.builder().token('7824250935:AAEDJ0gWhe1jsIhybpDA1cgY9I
 # Add a command handler for the '/start' command
 application.add_handler(CommandHandler('start', start))
 application.add_handler(CommandHandler('meme',meme))
+application.add_handler(CommandHandler('youtube video download',download))
 
 
 # Start polling for updates
