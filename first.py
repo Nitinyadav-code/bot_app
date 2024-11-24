91203d03147421eedf324bb48606cef89dedaa30
Nitinyadav-code <168860833+Nitinyadav-code@users.noreply.github.com>
2024-11-25 00:20:14 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,23 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+def 
+
+# Define the command handler function
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Welcome to my mini-app!  write  /meme for meme')
+async def meme(update:Update, context: ContextTypes.DEFAULT_TYPE)-> None:
+    await update.message.reply_text("serach @hasle_kallu on instagram" )
+    
+
+
+# Initialize the Application with the bot token
+application = Application.builder().token('7824250935:AAEDJ0gWhe1jsIhybpDA1cgY9IWRUPjgmps').build()
+
+# Add a command handler for the '/start' command
+application.add_handler(CommandHandler('start', start))
+application.add_handler(CommandHandler('show',meme))
+
+
+# Start polling for updates
+application.run_polling()
